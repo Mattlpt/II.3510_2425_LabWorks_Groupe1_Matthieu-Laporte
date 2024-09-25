b284bf3cfe8c1aa978b250dd54f20e4830828568
Mattlpt <matthieu.lpt56@gmail.com>
2024-09-25 12:28:50 +0200

    view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,67 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var textViewDisplay: TextView
+    private lateinit var buttonSubmit: Button
+    private lateinit var buttonChangeColor: Button
+    private lateinit var buttonIncreaseSize: Button
+    private lateinit var buttonDecreaseSize: Button
+
+    private var isColorRed = false
+    private var textSize = 16f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        textViewDisplay = findViewById(R.id.textViewDisplay)
+        buttonSubmit = findViewById(R.id.buttonSubmit)
+        buttonChangeColor = findViewById(R.id.buttonChangeColor)
+        buttonIncreaseSize = findViewById(R.id.buttonIncreaseSize)
+        buttonDecreaseSize = findViewById(R.id.buttonDecreaseSize)
+
+        buttonSubmit.setOnClickListener {
+            val inputText = editText.text.toString()
+            textViewDisplay.text = inputText
+        }
+
+        buttonChangeColor.setOnClickListener {
+            if (isColorRed) {
+                textViewDisplay.setTextColor(Color.BLACK)
+            } else {
+                textViewDisplay.setTextColor(Color.RED)
+            }
+            isColorRed = !isColorRed
+        }
+
+        buttonIncreaseSize.setOnClickListener {
+            textSize += 2f
+            textViewDisplay.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize)
+        }
+
+        buttonDecreaseSize.setOnClickListener {
+            if (textSize > 10f) {
+                textSize -= 2f
+                textViewDisplay.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize)
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,3 +69,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
